abf1a06e2c6c7ee9d9f3eccfa001894b48133391
filip.rastovic@thinkcube.solutions <filip.rastovic@thinkcube.solutions>
2018-10-02 20:56:31 +0200

    -justo updates

--- landing-pages/justo/js/creative.js
+++ landing-pages/justo/js/creative.js
@@ -91,4 +91,14 @@
     }
   });
 
+  window.onscroll = function() {scrollFunction();};
+
+  function scrollFunction() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          document.getElementById("myBtn").style.display = "block";
+      } else {
+          document.getElementById("myBtn").style.display = "none";
+      }
+  }
+  
 })(jQuery); // End of use strict
