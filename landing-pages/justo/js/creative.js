7ab82e474d7ac60d50d20b4492d76bac017f1139
filip.rastovic@thinkcube.solutions <filip.rastovic@thinkcube.solutions>
2018-10-03 14:32:09 +0200

    -modalfixes

--- landing-pages/justo/js/creative.js
+++ landing-pages/justo/js/creative.js
@@ -77,3 +77,30 @@
   
 })(jQuery); // End of use strict
 
+$(function () {
+
+  // init the validator
+  // validator files are included in the download package
+  // otherwise download from http://1000hz.github.io/bootstrap-validator
+
+  // when the form is submitted
+  $('#contact-form-1').on('submit', function (e) {
+
+      // if the validator does not prevent form submit
+      if (!e.isDefaultPrevented()) {
+          var url = "contact.php";
+
+          // POST values in the background the the script URL
+          $.ajax({
+              type: "POST",
+              url: url,
+              data: $(this).serialize(),
+              success: function (data)
+              {
+                
+              }
+          });
+          return false;
+      }
+  })
+});
